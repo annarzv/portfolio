6bc397aededa36739a8419a9597fa8c2f5eee251
anna <annabrizvi@gmail.com>
2025-07-05 16:25:18 -0400

    screensaver upload
    initial upload of screensaver

---
+++ screensaver/main.js
@@ -0,0 +1,9 @@
+// Dark Mode for background + button
+$(".dark-btn").click(function () {
+  $("body").toggleClass("dark");
+  $("button").toggleClass("dark");
+});
+
+// $(".light-btn").click(function () {
+//   $("body").removeClass("dark");
+// });
